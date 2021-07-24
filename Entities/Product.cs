39c51e76c1dce9d77f283556094e19e7a40f7953
k.pouria7@gmail.com <k.pouria7@gmail.com>
2021-07-24 22:38:58 +0430

    Data Annotations

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
+    [Table("productts",Schema ="prod")]
    public class Product
     {
+        //[Key] //set primary key
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)] not Automatic Generate
         public int Id { get; set; }
+        [Column(TypeName ="varchar(50)")]
         public string Name { get; set; }
+        [Column("pricee")]
+        [Required]
         public int Price { get; set; }
+
+        [NotMapped]
+        public int TotalPrice { get; set; }
     }
 }
