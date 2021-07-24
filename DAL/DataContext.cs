4226f5317e670372b425ba09de35998bb0896b2b
k.pouria7@gmail.com <k.pouria7@gmail.com>
2021-07-24 22:09:46 +0430

    Add DataContext And product(efcore)

---
+++ DAL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class DataContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = .\SQL2019;Initial Catalog=EntitiFrameWorkCoreCourse;Integrated Security=true");
+        }
+    }
+}
---
+++ EfCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using System;
+
+namespace EfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext context = new DataContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); 
+        }
+    }
+}
